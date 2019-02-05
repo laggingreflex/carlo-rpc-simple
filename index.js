2d004c9a9bdf5845023ba8ae9ab369e8e778a8e3
laggingreflex <laggingreflex@gmail.com>
2019-02-05 06:04:47 +0530

    feat: id, loadParams checks/returns/passes params

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-const isBrowser = typeof carlo !== 'undefined';
-
 module.exports = class RPC {
-  constructor() {
+  constructor({ id = 'carlo-rpc-simple' } = {}) {
+    this.id = id;
     this._remoteSetDefer = defer();
+    this.isBrowser = typeof carlo !== 'undefined';
   }
 
-  loadParams() {
-    if (isBrowser) return carlo.loadParams().then(([remote]) => {
+  loadParams(...params) {
+    if (this.isBrowser) return carlo.loadParams().then(async (params) => {
+      let remote;
+      for (const param of params) {
+        if (typeof param.id !== 'function') continue;
+        const id = await param.id();
+        if (id === this.id) {
+          if (remote) throw new Error("Multiple remotes using `{id: '${this.id}'}` found. Please set a unique `id`");
+          remote = param;
+        }
+      }
+      if (!remote) throw new Error("Couldn't find a valid remote object in arguments passed to `loadParams`. Please make sure to call `loadParams` on remote");
       this.setRemote(remote);
       remote.setRemote(rpc.handle(this));
+      return params.filter(p => p !== remote);
     });
     else {
       let rpc;
-      try { rpc = eval('require')('carlo/rpc').rpc } catch (error) { if (!isBrowser) throw error }
-      return [rpc.handle(this)];
+      try { rpc = eval('require')('carlo/rpc').rpc } catch (error) { if (!this.isBrowser) throw error }
+      return [rpc.handle(this), ...params];
     }
   }
 
   setRemote(remote) {
     this.remote = remote;
     this._remoteSetDefer.resolve();
-    console.log('[Carlo RPC] remote ready');
+    console.log(`[Carlo RPC](${this.id}) remote ready`);
   }
 
   get remoteReady() { return this._remoteSetDefer.promise }
